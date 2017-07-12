2c594c45798053d9fa871fd053e5997efb8ab207
HarshaRani <ranishashi@gmail.com>
2017-07-12 10:49:05 +0530

    in gui stoich.status along with warning, which Reac and Enz are dangling is also displayed in the QMessageBox

--- plugins/default.py
+++ plugins/default.py
@@ -709,19 +709,34 @@ class SchedulingWidget(QtGui.QWidget):
         else:
             stoich = moose.Stoich(compt[0].path+'/stoich')
             status = int(stoich.status)
+            # Flag to track status of Stoich object.
+            # -1: No path yet assigned.
+            # 0: Success
+            # 1: Warning: Missing reactant in Reac or Enz
+            # 2: Warning: Missing substrate in MMenz
+            # 4: Warning: Compartment not defined
+            # 8: Warning: Neither Ksolve nor Dsolve defined
+            # 16: Warning: No objects found on path
+
             # print("Status =>", status)
+
+            if status == 1 or status == 2:
+                nameRE = "\n\nclassName --> parent --> name  "
+                for res in moose.wildcardFind(self.modelRoot+'/##[ISA=ReacBase],'+self.modelRoot+'/##[ISA=EnzBase]'):
+                    if not len(res.neighbors["sub"]) or not len(res.neighbors["prd"]):
+                        nameRE = nameRE+"\n "+res.className + " --> "+res.parent.name+ " --> "+res.name
+
             if status == -1:
                 QtGui.QMessageBox.warning(None,"Could not Run the model","Warning: Reaction path not yet assigned.\n ")
                 return -1
             if status == 1:
-                QtGui.QMessageBox.warning(None,"Could not Run the model","Warning: Missing a reactant in a Reac or Enz.\n ")
+                #QtGui.QMessageBox.warning(None,"Could not Run the model","Warning: Missing a reactant in a Reac or Enz.\n ")
+                QtGui.QMessageBox.warning(None,"Could not Run the model","Warning: Missing a reactant in %s " %(nameRE))
                 return 1
             elif status == 2:
-                QtGui.QMessageBox.warning(None,"Could not Run the model","Warning: Missing a substrate in an MMenz.\n ")
+                QtGui.QMessageBox.warning(None,"Could not Run the model","Warning: Missing a substrate in an MMenz %s " %(nameRE))
+                #QtGui.QMessageBox.warning(None,"Could not Run the model","Warning: Missing a substrate in an MMenz.\n ")
                 return 2
-            elif status == 3:
-                QtGui.QMessageBox.warning(None,"Could not Run the model","Warning: Missing substrates as well as reactants.\n ")
-                return 3
             elif status == 4:
                 QtGui.QMessageBox.warning(None,"Could not Run the model"," Warning: Compartment not defined.\n ")
                 return 4
